f28a896029e55425361988c3bb5667562e4d9802
Calvin Chan <xlysergia@gmail.com>
2024-03-04 21:49:38 +0000

    ah

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Get the directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Source the virtual environment activation script
-source venv/bin/activate
+source "$DIR/venv/bin/activate"
 
 # Start Gunicorn with the specified options
-gunicorn -w 4 -b 0.0.0.0:5000 --timeout 120 wsgi:app.py
+gunicorn -w 4 -b 0.0.0.0:5000 --timeout 120 wsgi:app
