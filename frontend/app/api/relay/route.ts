96027e2c7ac9f446602322e11924453a508ed5f0
Calvin Chan <xlysergia@gmail.com>
2024-03-05 09:10:42 +0000

    what about now

--- frontend/app/api/relay/route.ts
+++ frontend/app/api/relay/route.ts
@@ -1,6 +1,6 @@
 import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
-import { NEXT_PUBLIC_URL, NEXT_API_URL, NEXT_API_URL_WITHOUTPORT } from '../../config';
+import { NEXT_PUBLIC_URL, NEXT_API_URL } from '../../config';
 import { NeynarAPIClient, isApiErrorResponse } from "@neynar/nodejs-sdk";
 import axios from "axios";
 
@@ -96,9 +96,9 @@ export async function POST(req: NextRequest): Promise<Response> {
         }
       ],
       image: {
-        src: `${NEXT_API_URL_WITHOUTPORT}/static/filename`,
+        src: `${NEXT_API_URL}/static/filename`,
       },
-      postUrl: `${NEXT_API_URL_WITHOUTPORT}/static/filename`,
+      postUrl: `${NEXT_API_URL}/static/filename`,
     }),
   );
 
