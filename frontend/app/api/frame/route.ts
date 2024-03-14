9d8767d89e2d9c493cd6ad3519576600c6465670
Calvin Chan <xlysergia@gmail.com>
2024-03-14 00:51:20 +0000

    graphics

--- frontend/app/api/frame/route.ts
+++ frontend/app/api/frame/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: NextRequest): Promise<Response> {
         }
       ],
       image: {
-        src: `${NEXT_PUBLIC_URL}/kwentize.png`,
+        src: `${NEXT_PUBLIC_URL}/kwentize_1.png`,
       },
       postUrl: `${NEXT_PUBLIC_URL}/api/relay`,
     }),
