d71a339007096643b5e27f5ea81561d89b7d5ba4
Calvin Chan <xlysergia@gmail.com>
2024-03-19 01:20:42 +0000

    fixed ip

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,3 +1,3 @@
 export const NEXT_PUBLIC_URL = 'https://kwentize-frame.vercel.app';
-export const NEXT_API_URL = 'http://3.21.66.28:5000'
-export const NEXT_API_URL_WITHOUTPORT = 'http://3.21.66.28'
+export const NEXT_API_URL = 'http://3.128.16.10:5000'
+export const NEXT_API_URL_WITHOUTPORT = 'http://3.128.16.10'
